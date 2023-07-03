d74523d559ecdfb2d596328fabacc062fca444e2
DiodeCN <1069523953@qq.com>
2023-07-03 20:10:08 +0800

    Initial commit

---
+++ src/main/java/cn/diodecn/MyOKR/demo/MyOkrBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.diodecn.MyOKR.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOkrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOkrBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/diodecn/MyOKR/demo/MyOkrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.diodecn.MyOKR.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOkrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
