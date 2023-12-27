fc2ab53b92a46feeb99025715a00865d8e96fb9e
ElmCose <1069523953@qq.com>
2023-12-27 19:04:50 +0800

    连接成功: 连接成功

---
+++ src/main/java/cn/diodecn/MyOKR/demo/controller/ApiController.java
@@ -0,0 +1,16 @@
+package cn.diodecn.MyOKR.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/api/connect")
+    @CrossOrigin(origins = "http://localhost:5173") // 允许来自特定源的跨域请求
+    public String connect() {
+        return "连接成功";
+    }
+}
---
+++ src/main/java/cn/diodecn/MyOKR/demo/controller/MyWebSocketHandler.java
@@ -0,0 +1,16 @@
+package cn.diodecn.MyOKR.demo.controller;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class MyWebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String receivedMessage = message.getPayload();
+        if ("test".equals(receivedMessage)) {
+            session.sendMessage(new TextMessage("ok"));
+        }
+    }
+}
